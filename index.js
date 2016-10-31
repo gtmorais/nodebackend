c003fda19abe6330219f4c3986d5d477a9a778d4
wenzhongzheng <andrew198712@gmail.com>
2016-10-31 12:09:47 -0400

    testing push to master

--- index.js
+++ index.js
@@ -1,92 +1,109 @@
-
-var SERVER_NAME = 'myApi'
+var SERVER_NAME = 'eTouchCare'
 var PORT = 3000;
 var HOST = '127.0.0.1';
 
 
 var restify = require('restify')
-var save = require('save')('products')
-var server = restify.createServer({ name: SERVER_NAME})
-var sendGetCount = 0, sendPostCount = 0, sendPutCount = 0
+
+  // Get a persistence engine for the patients
+  , patientsSave = require('save')('patients')
+
+  // Create the restify server
+  , server = restify.createServer({ name: SERVER_NAME})
 
   server.listen(PORT, HOST, function () {
   console.log('Server %s listening at %s', server.name, server.url)
- 
-  console.log('Endpoints:')
-  console.log('http://127.0.0.1:3000/products method: GET')
-  console.log('http://127.0.0.1:3000/products/edit method: POST')
-  console.log('http://127.0.0.1:3000/products/edit/ method: PUT')
+  console.log('Resources:')
+  console.log(' /patients')
+  console.log(' /patients/:id')  
 })
 
 server
+  // Allow the use of POST
   .use(restify.fullResponse())
+
+  // Maps req.body to req.params so there is no switching between them
   .use(restify.bodyParser())
 
-// List all method
-server.get('/products', function (req, res, next) {
-    sendGetCount++
-    console.log("sendGet: received request. Request counts: " + sendGetCount)
+// Get all patients in the system
+server.get('/patients', function (req, res, next) {
 
-    save.find({}, function (error, allItems) {
+  // Find every entity within the given collection
+  patientsSave.find({}, function (error, patients) {
 
-    console.log("sendGet: sending response")
-    res.send(allItems)
+    // Return all of the patients in the system
+    res.send(patients)
   })
 })
 
-// Create a new item
-server.post('/products/edit', function (req, res, next) {
-  sendPostCount++
-  console.log("sendPost: received request. Request counts: " + sendPostCount)
+// Get a single user by their user id
+server.get('/patients/:id', function (req, res, next) {
 
-  // Make sure name is defined
-  if (req.params.product === undefined || req.params.price === undefined) {
-    // If there are any errors, pass them to next in the correct format
-    return next(new restify.InvalidArgumentError('The parameters Product and Price must be supplied'))
-  }
-  
-  if (req.params.price === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(new restify.InvalidArgumentError('price must be supplied'))
-  }
-  
-   var newProd = {
-		name: req.params.product, 
-		price: req.params.price
-	}
+  // Find a single user by their id within save
+  patientsSave.findOne({ _id: req.params.id }, function (error, user) {
 
-  
-  save.create(newProd, function (error, newProd) {
     // If there are any errors, pass them to next in the correct format
     if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-    
-    console.log("sendPost: sending response")
-    res.send(200)
+
+    if (user) {
+      // Send the user if no issues
+      res.send(user)
+    } else {
+      // Send 404 header if the user doesn't exist
+      res.send(404)
+    }
   })
 })
 
-// Update a product by their id
-server.put('/products/edit/:id', function (req, res, next) {
-  sendPutCount++
-  console.log("sendPut: received request. Request counts: " + sendPutCount)
+// Create a new user
+server.post('/patients', function (req, res, next) {
+
   // Make sure name is defined
-  if (req.params.product === undefined ) {
+  if (req.params.name === undefined ) {
     // If there are any errors, pass them to next in the correct format
     return next(new restify.InvalidArgumentError('name must be supplied'))
   }
-  if (req.params.price === undefined ) {
+  if (req.params.age === undefined ) {
     // If there are any errors, pass them to next in the correct format
-    return next(new restify.InvalidArgumentError('price must be supplied'))
+    return next(new restify.InvalidArgumentError('age must be supplied'))
+  }
+  var newUser = {
+		name: req.params.name, 
+		age: req.params.age
+	}
+
+  // Create the user using the persistence engine
+  patientsSave.create( newUser, function (error, user) {
+
+    // If there are any errors, pass them to next in the correct format
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+
+    // Send the user if no issues
+    res.send(201, user)
+  })
+})
+
+// Update a user by their id
+server.put('/patients/:id', function (req, res, next) {
+
+  // Make sure name is defined
+  if (req.params.name === undefined ) {
+    // If there are any errors, pass them to next in the correct format
+    return next(new restify.InvalidArgumentError('name must be supplied'))
+  }
+  if (req.params.age === undefined ) {
+    // If there are any errors, pass them to next in the correct format
+    return next(new restify.InvalidArgumentError('age must be supplied'))
   }
   
-  var newProd = {
+  var newUser = {
 		_id: req.params.id,
-		name: req.params.product, 
-		price: req.params.price
+		name: req.params.name, 
+		age: req.params.age
 	}
   
-  // Update the product with the persistence engine
-  save.update(newProd, function (error, newProd) {
+  // Update the user with the persistence engine
+  patientsSave.update(newUser, function (error, user) {
 
     // If there are any errors, pass them to next in the correct format
     if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
@@ -96,5 +113,18 @@ server.put('/products/edit/:id', function (req, res, next) {
   })
 })
 
+// Delete user with the given id
+server.del('/patients/:id', function (req, res, next) {
+
+  // Delete the user with the persistence engine
+  patientsSave.delete(req.params.id, function (error, user) {
+
+    // If there are any errors, pass them to next in the correct format
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+
+    // Send a 200 OK response
+    res.send()
+  })
+})
 
 
